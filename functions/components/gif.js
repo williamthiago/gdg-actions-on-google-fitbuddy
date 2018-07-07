d6ab6c5e6acef3686f8fd85f5458d619cb5349fb
William Thiago <williamthiago@gmail.com>
2018-07-07 18:43:40 -0400

    Change gif size

--- functions/components/gif.js
+++ functions/components/gif.js
@@ -3,6 +3,6 @@ const config = require('../config');
 
 const fetchGif = (text) => fetch(`http://api.giphy.com/v1/gifs/translate?api_key=${config.GIPHY_KEY}&s=${text}`)
     .then(res => res.json())
-    .then(res => res.data.images.fixed_height_downsampled.url);
+    .then(res => res.data.embed_url);
 
 module.exports = fetchGif;
