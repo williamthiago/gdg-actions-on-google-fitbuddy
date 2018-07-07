ca14fc66c8360af092b7ff7717e1d62ab0c5761e
William Thiago <williamthiago@gmail.com>
2018-07-07 19:05:06 -0400

    Add user body info and consume handlers

--- functions/intents/index.js
+++ functions/intents/index.js
@@ -1,7 +1,11 @@
 const welcomeHandler = require('./welcomeHandler');
 const fallbackHandler = require('./fallbackHandler');
+const userConsumeHandler = require('./userConsumeHandler');
+const userBodyInfoHandler = require('./userBodyInfoHandler');
 
 module.exports = {
     welcomeHandler,
-    fallbackHandler
+	fallbackHandler,
+	userBodyInfoHandler,
+	userConsumeHandler
 };
