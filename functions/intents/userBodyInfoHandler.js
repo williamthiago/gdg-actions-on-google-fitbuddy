a4d7911200fe8cbe421d97023c6cf9288b1b6928
William Thiago <williamthiago@gmail.com>
2018-07-07 19:43:36 -0400

    Add logs on user body handler

--- functions/intents/userBodyInfoHandler.js
+++ functions/intents/userBodyInfoHandler.js
@@ -6,17 +6,35 @@ const userBodyInfoHandler = assistant => {
 	console.log(assistant.parameters);
 	assistant.ask(strings.USERBODY.MESSAGES.ALL_SET);
 
-	let imc = calcImc(1.9, 60);
-	let calories = calcCalories(1.9, 60, 30, 'sedentary', 'male');
+	const {
+		age: { amount: age },
+		weight: { amount: weight },
+		height: { amount: height },
+		gender,
+		lifeStyle
+	} = assistant.parameters;
+
+	console.log(age, weight, height, gender, lifeStyle);
+
+	let imc = calcImc(height, weight);
+	let calories = calcCalories(height, weight, age, lifeStyle, gender);
 	let shape = calcShape(imc);
 
+	console.log('calories' + calories);
+	console.log('imc' + imc);
+	console.log('shape' + shape);
+
 	assistant.ask(strings.USERBODY.MESSAGES.AMOUNT_OF_CALORIES.replace(/_CALORIES_/g, calories));
+
+	console.log(strings.USERBODY.MESSAGES.AMOUNT_OF_CALORIES.replace(/_CALORIES_/g, calories));
 
 	let messagesByShape = {
 		'skinny': strings.USERBODY.MESSAGES.SKINNY_GOALS,
 		'overweight': strings.USERBODY.MESSAGES.OVERWEIGHT_GOALS,
 		'normal': strings.USERBODY.MESSAGES.NORMAL_GOALS
 	};
+
+	console.log(messagesByShape[shape]);
 
 	assistant.ask(messagesByShape[shape]);
 };
