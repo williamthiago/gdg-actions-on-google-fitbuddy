8911ebed8c248797ab87dc16a08dc3c4d9fa6182
castrolol <lu4n.k4str0@gmail.com>
2018-07-07 20:08:32 -0400

    Add user consume start

--- functions/intents/userConsumeHandler.js
+++ functions/intents/userConsumeHandler.js
@@ -1,5 +1,26 @@
-const userConsumeHandler = assistant => {
-  assistant.ask('User consume respondendo');
+function getCaloriesOf(food) {
+	return 100;
+}
+
+function sumConsumablesCalories(consumables) {
+	let total = 0;
+
+	for (var i = 0; i < consumables; i++) {
+		const amount = consumables[i].amount || 1;
+		const calories = getCaloriesOf(consumables.consumable);
+
+		total += calories;
+	}
+
+	return total;
+}
+
+const userConsumeHandler = (assistant) => {
+	const { date, meal, consumables } = assistant.parameters;
+
+	sumConsumablesCalories(consumables);
+
+	assistant.ask(`Your meal have about ${totalCalories} calories`);
 };
 
 module.exports = userConsumeHandler;
